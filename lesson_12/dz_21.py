2030540ebcd6db6ab0e79ac0f585d1dc77e6a3c3
fizevizl <dnruban@gmail.com>
2023-10-02 17:41:37 +0200

    update hw 21

--- lesson_12/dz_21.py
+++ lesson_12/dz_21.py
@@ -8,10 +8,18 @@ class Auto:
         self.weight = weight
 
     def move(self):
-        print("move")
+        print(f"{self.brand} is moving.")
+
+    def stop(self):
+        print(f"{self.brand} has stopped.")
 
     def birthday(self):
         self.age += 1
 
-    def stop(self):
-        print("stop")
+# Приклад використання класу Auto:
+car1 = Auto(brand='Toyota', age=3, mark='Camry', color='red', weight=1500)
+print(f"Car 1: Brand - {car1.brand}, Age - {car1.age}, Mark - {car1.mark}, Color - {car1.color}, Weight - {car1.weight}")
+car1.move()
+car1.stop()
+car1.birthday()
+print(f"Car 1's age after birthday: {car1.age}")
