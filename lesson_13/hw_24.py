a6fa94e73aa5427a414e6b2449d906fc5154f43c
fizevizl <dnruban@gmail.com>
2023-10-08 15:36:59 +0200

    new hw 24

---
+++ lesson_13/hw_24.py
@@ -0,0 +1,35 @@
+class String(str):
+    def __init__(self, value):
+        self.value = str(value)
+
+    def __add__(self, other):
+        if not isinstance(other, String):
+            other = String(other)
+        return String(super().__add__(other))
+    
+    def __sub__(self, other):
+        if not isinstance(other, String):
+            other = String(other)
+        return String(self.value.replace(other, "", 1))
+
+    
+a = String('New')
+b = String(890)
+
+print(String('New bala7nce') - 7              ) 
+print(String('New balance') - 'bal'           ) 
+print(String('New balance') - 'Bal'           ) 
+print(String('pineapple apple pine') - 'apple')
+print(String('New balance') - 'apple')          
+print(String('NoneType') - None      )          
+print(String(55678345672) - 7        )          
+
+
+# print(a + b, type(a + b))   
+# print(String('New') + String(890))   
+# print(String(1234) + 5678           )
+# print(String('New') + 'castle'      )
+# print(String('New') + 77            )
+# print(String('New') + True         )
+# print(String('New') + ['s', ' ', 23])
+# print(String('New') + None         )
